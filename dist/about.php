7e8e93706d48d104250f76ba85840ab30667ff3d
DimitrijevasDominykas <dimitrijevas@gmail.com>
2017-11-16 21:37:11 +0200

    first commit

---
+++ dist/about.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+   <?php include 'parts/head.php' ?>
+   <body>
+       <?php include 'parts/header.php' ?>
+       
+       <?php include 'parts/footer.php' ?>
+   </body>
+    
+</html>
